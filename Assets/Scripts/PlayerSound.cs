e10880e8b48053ce9aa987711c3eb47d815843b9
Randyh-25 <randy.122140171@student.itera.ac.id>
2025-11-18 20:11:58 +0700

    "Beda pijakan beda suara" ahh commit...

--- Assets/Scripts/PlayerSound.cs
+++ Assets/Scripts/PlayerSound.cs
@@ -3,32 +3,84 @@ using UnityEngine;
 public class PlayerSound : MonoBehaviour
 {
     public AudioSource audioSource;
-    public AudioClip[] walkSounds;
-    public AudioClip[] runSounds;
+    public SurfaceSound[] surfaceSounds;
 
+    private string currentSurfaceTag = "DirtyGround";
     private AudioClip lastWalkSound;
     private AudioClip lastRunSound;
+    private AudioClip lastJumpStartSound;
+    private AudioClip lastJumpEndSound;
+
+    void FixedUpdate()
+    {
+        if (Physics.Raycast(transform.position, Vector3.down, out RaycastHit hit, 3f))
+        {
+            currentSurfaceTag = hit.collider.tag;
+        }
+    }
 
     public void PlayWalkSound()
     {
-        if (walkSounds.Length > 0)
+        SurfaceSound surfaceSound = GetCurrentSurfaceSound();
+        if (surfaceSound != null && surfaceSound.walkClips.Length > 0)
         {
-            AudioClip clipToPlay = GetRandomClip(walkSounds, lastWalkSound);
+            AudioClip clipToPlay = GetRandomClip(surfaceSound.walkClips, lastWalkSound);
             lastWalkSound = clipToPlay;
-            audioSource.clip = clipToPlay;
-            audioSource.Play();
+            audioSource.PlayOneShot(clipToPlay);
         }
     }
 
     public void PlayRunSound()
     {
-        if (runSounds.Length > 0)
+        SurfaceSound surfaceSound = GetCurrentSurfaceSound();
+        if (surfaceSound != null && surfaceSound.runClips.Length > 0)
         {
-            AudioClip clipToPlay = GetRandomClip(runSounds, lastRunSound);
+            AudioClip clipToPlay = GetRandomClip(surfaceSound.runClips, lastRunSound);
             lastRunSound = clipToPlay;
-            audioSource.clip = clipToPlay;
-            audioSource.Play();
+            audioSource.PlayOneShot(clipToPlay);
         }
+    }
+
+    public void PlayJumpStartSound()
+    {
+        SurfaceSound surfaceSound = GetCurrentSurfaceSound();
+        if (surfaceSound != null && surfaceSound.jumpStartClips.Length > 0)
+        {
+            AudioClip clipToPlay = GetRandomClip(surfaceSound.jumpStartClips, lastJumpStartSound);
+            lastJumpStartSound = clipToPlay;
+            audioSource.PlayOneShot(clipToPlay);
+        }
+    }
+
+    public void PlayJumpEndSound()
+    {
+        SurfaceSound surfaceSound = GetCurrentSurfaceSound();
+        if (surfaceSound != null && surfaceSound.jumpEndClips.Length > 0)
+        {
+            AudioClip clipToPlay = GetRandomClip(surfaceSound.jumpEndClips, lastJumpEndSound);
+            lastJumpEndSound = clipToPlay;
+            audioSource.PlayOneShot(clipToPlay);
+        }
+    }
+
+    private SurfaceSound GetCurrentSurfaceSound()
+    {
+        foreach (var surfaceSound in surfaceSounds)
+        {
+            if (surfaceSound.tag == currentSurfaceTag)
+            {
+                return surfaceSound;
+            }
+        }
+        // Return default if no tag is found
+        foreach (var surfaceSound in surfaceSounds)
+        {
+            if (surfaceSound.tag == "DirtyGround")
+            {
+                return surfaceSound;
+            }
+        }
+        return null;
     }
 
     private AudioClip GetRandomClip(AudioClip[] clips, AudioClip lastClip)
